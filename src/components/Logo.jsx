892aca860f3dfb6ca1c949ae200d47284c894006
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-20 18:55:46 +0200

    logo dir

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Logo(props) {
   return (
-    <div>
+    <div >
       {props.type == "muted" ? (
         <img src='images/logo-muted.svg' alt='logo'></img>
       ) : props.type == null ? (

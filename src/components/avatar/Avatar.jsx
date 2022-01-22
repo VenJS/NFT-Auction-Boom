0cb79d04c3c303a3cc36b47ab90fadd6dc7bb742
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-23 00:49:03 +0200

    asdas

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,11 +1,12 @@
 import styles from './Avatar.module.scss'
 import classNames from 'classnames'
 
-export default function Avatar({size='90px', url, verified='false'}) {
+export default function Avatar({size, url, verified}) {
     return (
-        <div className={classNames(styles.avatar)} style={{width: size, height: size}} ver={verified}>
+        <div className={classNames(styles.avatar)} style={{width: size || '90px', height: size || '90px'}} ver={verified}>
             <img className={classNames(styles.image)} src={url} ></img>
             <img className={classNames(styles.badge)} src='images/verified.svg'></img>
         </div>
     )
+    
 }

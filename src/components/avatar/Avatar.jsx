944a0eda84749e58b2418c174dd01186db5110ef
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-23 00:02:31 +0200

    size fixed

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,9 +3,9 @@ import classNames from 'classnames'
 
 export default function Avatar({size, url, verified}) {
     return (
-        <container className={classNames(styles.avatar)} style={{width: {size}, height: {size}}} verified={verified}>
+        <div className={classNames(styles.avatar)} style={{width: size, height: size}} verified={verified}>
             <img className={classNames(styles.image)} src={url} ></img>
-            {verified == 'true' ? <img className={classNames(styles.badge)} src='images/verified.svg'></img> : null }
-        </container>
+            <img className={classNames(styles.badge)} src='images/verified.svg'></img>
+        </div>
     )
 }

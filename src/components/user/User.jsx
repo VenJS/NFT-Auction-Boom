cd08efe9e31a154cc25a9fef10fc77f854f5ae7e
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-23 02:33:29 +0200

    dsfsdfds

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -5,8 +5,8 @@ import classNames from "classnames"
 export default function User({name='', info='', avatar='', size=55, verified=false}) {
     return (
         <div className={classNames(styles.user)}>
-            <div className={classNames(styles.user_name)}>{name}</div>
-            <div className={classNames(styles.user_info)}>{info}</div>
+            <div className={classNames(styles.name)}>{name}</div>
+            <div className={classNames(styles.info)}>{info}</div>
             <Avatar url='images/avatar.png' />
         </div>
     )

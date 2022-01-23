0f1fdaccfdf39e1c2d49d5eea7898e7e91974200
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-23 02:03:28 +0200

    user component added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Header from "../src/components/header/Header";
-import Avatar from "../src/components/avatar/Avatar";
+import User from "../src/components/user/User";
 
 export default function Index() {
   return <div><Header />
-    <Avatar url='images/avatar.png' verified='false' />
+    <User  />
   </div>;
 }

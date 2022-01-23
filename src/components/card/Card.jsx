15e5053d9a479fb0be517471b5e4d5eeac30ff15
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-24 00:18:30 +0200

    countdown edited

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,11 +5,13 @@ import Chip from "@mui/material/Chip";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
 import { Favorite } from "@mui/icons-material";
+import Countdown from "react-countdown";
 
 export default function Card({
   name = "String",
   likes = 0,
   mediaUrl = "images/nft.jpg",
+  timeLeft = 1000000,
   user = {
     avatar: {
       url: "String",
@@ -25,6 +27,11 @@ export default function Card({
         <Avatar url="images/avatar.png" size="55px" />
         <img className={classNames(styles.media)} src={mediaUrl} />
         <div className={classNames(styles.container)}>
+          {
+            <div className={classNames(styles.countdown)}>
+              <Countdown date={Date.now() + 1000000}></Countdown>
+            </div>
+          }
           <div className={classNames(styles.title)}>{name}</div>
           <Chip
             className={classNames(styles.likes)}
@@ -32,7 +39,9 @@ export default function Card({
             clickable
             label={millify(likes)}
           ></Chip>
-          <div className={classNames(styles.price)}>{price} {currency}</div>
+          <div className={classNames(styles.price)}>
+            {price} {currency}
+          </div>
         </div>
       </CardTag>
     </div>
--- src/components/live card/LiveCard.jsx
+++ src/components/live card/LiveCard.jsx
@@ -1,5 +1,4 @@
 import Card from "../card/Card";
-import Countdown from "react-countdown";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import classNames from "classnames";
 import styles from './LiveCard.module.scss'
@@ -11,10 +10,7 @@ export default function LiveCard() {
         <FiberManualRecordIcon></FiberManualRecordIcon>
         <span>LIVE</span>
       </div>
-      <Card timeLeft="10"></Card>
-      <div className={classNames(styles.countdown)}>
-      <Countdown date={Date.now() + 1000000}></Countdown>
-      </div>
+      <Card></Card>
     </div>
   );
 }

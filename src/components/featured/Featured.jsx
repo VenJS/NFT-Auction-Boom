e37109b6484b5a9f24b98bc5ba29fa8e52d93318
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-25 17:48:42 +0200

    first image bigger than others fixed

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,64 +4,62 @@ import styles from "./Featured.module.scss";
 import { useRouter } from "next/router";
 
 export default function Featured({
-  items = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
-      title: "Breakfast",
-      rows: 2,
-      cols: 3,
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
-      title: "Burger",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
-      title: "Camera",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
-      title: "Coffee",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
-      title: "Hats",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
-      title: "Honey",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
-      title: "Basketball",
-      href: "/about",
-    },
-  ],
+  items = [{
+    image:
+      "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
+    title: "Breakfast",
+    rows: 2,
+    cols: 3,
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
+    title: "Burger",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
+    title: "Camera",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
+    title: "Coffee",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
+    title: "Hats",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
+    title: "Honey",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
+    title: "Basketball",
+    href: "/about",
+  },],
 }) {
   const router = useRouter();
 
   return (
-    <div>
+    <div> {items == [] ? null :
       <ImageList
         sx={{ width: 500, height: 450 }}
-        cols={4}
+        cols={6}
         rowHeight={121}
       >
         {items.map((item) => (
-          <ImageListItem>
+          <ImageListItem cols={item.cols || 1}>
             <img
               src={item.image}
               alt={item.title}
@@ -69,7 +67,7 @@ export default function Featured({
             />
           </ImageListItem>
         ))}
-      </ImageList>
+      </ImageList>}
     </div>
   );
 }

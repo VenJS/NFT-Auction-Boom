6d956d38ca41964c48f078f592a104e3a2e6fa9b
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-26 18:32:51 +0200

    collectore type props fixed

--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -1,30 +1,31 @@
 import classNames from "classnames";
 import styles from "./CollectorColumn.module.scss";
 import Collector from "./Collector.jsx";
-export default function CollectorColumn() {
-  const items = [
-    {
-      name: "String",
-      nftsCount: "Number",
-      avatar: "String",
-      verified: "Boolean",
-      id: "Number",
-    },
-    {
-      name: "String",
-      nftsCount: "Number",
-      avatar: "String",
-      verified: "Boolean",
-      id: "Number",
-    },
-    {
-      name: "String",
-      nftsCount: "Number",
-      avatar: "String",
-      verified: "Boolean",
-      id: "Number",
-    },
-  ];
+export default function CollectorColumn({items = [
+  {
+    name: "String",
+    nftsCount: "Number",
+    avatar: "String",
+    verified: "Boolean",
+    id: "Number",
+  },
+  {
+    name: "String",
+    nftsCount: "Number",
+    avatar: "String",
+    verified: "Boolean",
+    id: "Number",
+  },
+  {
+    name: "String",
+    nftsCount: "Number",
+    avatar: "String",
+    verified: "Boolean",
+    id: "Number",
+  },
+]}) {
+  
+
   return (
     <div className={classNames(styles.container)}>
       {items.map((item, i) => (
@@ -33,7 +34,7 @@ export default function CollectorColumn() {
           item={item}
           index={i + 1}
           type={
-            i % 2 !== 0 ? classNames(styles.light) : classNames(styles.dark)
+            i % 2 !== 0 ? classNames(styles.light) : ''
           }
         />
       ))}

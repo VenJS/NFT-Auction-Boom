a445f84b49d239e573118ef423a2a6002aa0e822
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 00:33:34 +0200

    trying to fix the error with the classnames

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -2,11 +2,11 @@ import classNames from "classnames";
 import styles from "./ProductImage.module.scss";
 
 export default function ProductImage({
-  url = "https://miro.medium.com/max/1000/1*jKnY9OzEhllkSjq_W7zFbg.png",
+  url = "https://miro.medium.com/max/1000/1*jKnY9OzEhllkSjq_W7zFbg.png"
 }) {
   return (
-    <div className="product-image">
-      <img className="image" src={url} />
+    <div className={classNames(styles.product_image)}>
+      <img className={classNames(styles.image)} src={url} />
     </div>
   );
 }

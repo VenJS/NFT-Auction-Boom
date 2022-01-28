d96682e7562016648087fba2ba4f12a716f97c54
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 00:17:31 +0200

    props destructuring

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -1,12 +1,12 @@
 import classNames from "classnames";
 import styles from "./ProductImage.module.scss";
 
-export default function ProductImage(
-  url = "https://miro.medium.com/max/1000/1*jKnY9OzEhllkSjq_W7zFbg.png"
-) {
+export default function ProductImage({
+  url = "https://miro.medium.com/max/1000/1*jKnY9OzEhllkSjq_W7zFbg.png",
+}) {
   return (
-    <div className='product-image'>
-      <img className='image' src={url} />
+    <div className="product-image">
+      <img className="image" src={url} />
     </div>
   );
 }

45d6f4660d0b9d56e94888b053df0a2f4c685bcc
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-28 23:49:53 +0200

    ProductImage comp added

--- src/components/auctions/Auctions.jsx
+++ src/components/auctions/Auctions.jsx
@@ -27,16 +27,32 @@ export default function Auctions({ cards = [] }) {
       </Container>
       <Grid container spacing={1} className={classNames(styles.cardContainer)}>
         <Grid item xs={3}>
-          <Card name='Clock'></Card>
+          <Card name="Hollywood Reporter"
+            likes="188"
+            mediaUrl="https://www.hollywoodreporter.com/wp-content/uploads/2021/10/Mutant-Demon-Ape-Credit-0xb1-copy-H-2021.jpg?w=681&h=383&crop=1"
+            price = "~5.2"
+            currency = "SOL"></Card>
         </Grid>
         <Grid item xs={3}>
-          <Card name='DOGE'></Card>
+          <Card name="Bubbly"
+            likes="47"
+            mediaUrl="https://miro.medium.com/max/980/1*3iesg_sr8kC6NYN2iiFHRQ.png"
+            price = "~18.2"
+            currency = "BNB"></Card>
         </Grid>
         <Grid item xs={3}>
-          <Card name='BTC'></Card>
+          <Card name="Alien in New York"
+            likes="89"
+            mediaUrl="https://static.euronews.com/articles/stories/06/28/65/60/400x225_cmsv2_4ce73953-fe00-5c59-a04e-4a5edbb6f750-6286560.jpg"
+            price = "~11"
+            currency = "LTC"></Card>
         </Grid>
         <Grid item xs={3}>
-          <Card name="Litecoin"></Card>
+          <Card name="Reptile"
+            likes="411"
+            mediaUrl="https://www.thehindu.com/entertainment/art/ph95wn/article35362273.ece/alternates/FREE_615/Dhanya-Ajith2021Artontwitter"
+            price = "~8.2"
+            currency = "SOL"></Card>
         </Grid>
       </Grid>
     </div>

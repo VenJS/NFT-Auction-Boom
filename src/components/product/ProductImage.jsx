81a1677a96ad56c11f58146bd17bfd841f5e687a
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 15:42:40 +0200

    classnames fixed 3.0

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-
-import "./ProductImage.module.scss";
+import classNames from "classnames";
+import styles from "./ProductImage.module.scss";
 
 export default function ProductImage({
   url = "https://miro.medium.com/max/1000/1*jKnY9OzEhllkSjq_W7zFbg.png"
 }) {
   return (
-    <div className='product-image'>
-      <img className='image' src={url} />
+    <div className={classNames(styles['product-image'])}>
+      <img className={classNames(styles.image)} src={url} />
     </div>
   );
 }

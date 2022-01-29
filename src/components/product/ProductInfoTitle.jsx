c8489f38a6eb0b28ffb2a75c8b42c4e30573350c
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 17:10:24 +0200

    title prop typography

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -2,11 +2,11 @@ import classNames from "classnames";
 import styles from "./ProductInfoTitle.module.scss";
 import Typography from "@mui/material/Typography";
 
-export default function ProductInfoTitle({ text = {title: 'fdgfdgf'} }) {
+export default function ProductInfoTitle({ text, title='blabla' }) {
   return (
-    <div className={classNames(styles["product-info-title"])}>
-      <Typography className={classNames(styles.title)} color='primary'>
-        {text.title}
+    <div className={classNames(styles["product-info-title"]) } >
+      <Typography className={classNames(styles.title)} color='primary' >
+        {title}
       </Typography>
     </div>
   );

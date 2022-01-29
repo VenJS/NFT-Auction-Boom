5ff1a909e20a2194abfc7a66980be0005566ddae
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 22:12:28 +0200

    oncomplete timer

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -2,16 +2,24 @@ import classNames from "classnames";
 import styles from "./ProductInfoTimer.module.scss";
 import Countdown from "react-countdown";
 
-export default function ProductInfoTimer({ timeEnd = 0, onTimeEnd }) {
+export default function ProductInfoTimer({
+  timeEnd = 0,
+  onTimeEnd = () => <span>Auction ended!</span>,
+}) {
   return (
     <div className={classNames(styles["product-info-timer"])}>
       {timeEnd == null ? (
-        <div className={classNames(styles.timer)}>
-      </div>
+        <div className={classNames(styles.timer)}></div>
       ) : (
         <div className={classNames(styles.timer)}>
           <p className={classNames(styles.title)}>Ends In</p>
-          <Countdown className={classNames(styles.active)} date={Date.now() + 2500} >{onTimeEnd}</Countdown>
+          <Countdown
+            className={classNames(styles.active)}
+            date={Date.now() + 2500}
+            onComplete={() => {
+              return onTimeEnd();
+            }}
+          ></Countdown>
         </div>
       )}
     </div>

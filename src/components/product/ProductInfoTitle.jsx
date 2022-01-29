79cefc4724c53cb35cb2e52f110d19c04625b96e
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 16:51:43 +0200

    ProductInfoTitle update 4.0

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -6,7 +6,7 @@ export default function ProductInfoTitle({ text }) {
   return (
     <div className={classNames(styles["product-info-title"])}>
       <Typography className={classNames(styles.title)} color='primary'>
-        <p>{title}</p>
+        {text}
       </Typography>
     </div>
   );

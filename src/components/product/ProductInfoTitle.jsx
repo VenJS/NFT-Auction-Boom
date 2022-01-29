80b8417eec002453927b82df3f1307569e493a0e
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 16:31:25 +0200

    ProductInfoTitle update

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -1,11 +1,13 @@
 import classNames from "classnames";
-import styles from './ProductInfoTitle.module.scss';
-import Typography from '@mui/material/Typography';
+import styles from "./ProductInfoTitle.module.scss";
+import Typography from "@mui/material/Typography";
 
-export default function ProductInfoTitle({text}) {
-    return (
-        <div className={classNames(styles['product-info-title'])}>
-            <Typography className={classNames(styles.title)}>{title}</Typography>
-        </div>
-    )
+export default function ProductInfoTitle({ text }) {
+  return (
+    <div className={classNames(styles["product-info-title"])}>
+      <Typography className={classNames(styles.title)}>
+        <p>{title}</p>
+      </Typography>
+    </div>
+  );
 }

17e36b31bfbb58fe0f0ed943cd95b9d120c6dcad
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 16:12:43 +0200

    ProductInfoTitle comp added

---
+++ src/components/product/ProductInfoTitle.jsx
@@ -0,0 +1,11 @@
+import classNames from "classnames";
+import styles from './ProductInfoTitle.module.scss';
+import Typography from '@mui/material/Typography';
+
+export default function ProductInfoTitle({text}) {
+    return (
+        <div className={classNames(styles['product-info-title'])}>
+            <Typography className={classNames(styles.title)}>{text}</Typography>
+        </div>
+    )
+}

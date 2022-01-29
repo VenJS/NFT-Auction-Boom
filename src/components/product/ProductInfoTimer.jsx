9a8766fb49e5e425f16ef18f6ea617443a8fb35d
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-29 20:41:15 +0200

    Product info timer comp update

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -2,10 +2,18 @@ import classNames from "classnames";
 import styles from "./ProductInfoTimer.module.scss";
 import Countdown from "react-countdown";
 
-export default function ProductInfoTimer({ timeEnd = 21, onTimeEnd }) {
+export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
   return (
     <div className={classNames(styles["product-info-timer"])}>
-      {timeEnd == null ? <div></div> : <div className={classNames(styles.timer)}><Countdown date={Date.now() + 100000000}></Countdown></div>}
+      {timeEnd == null ? (
+        <div className={classNames(styles.timer)}>
+      </div>
+      ) : (
+        <div className={classNames(styles.timer)}>
+          <p className={classNames(styles.title)}>Ends In</p>
+          <Countdown className={classNames(styles.active)} date={Date.now() + 100000000}>{onTimeEnd}</Countdown>
+        </div>
+      )}
     </div>
   );
 }

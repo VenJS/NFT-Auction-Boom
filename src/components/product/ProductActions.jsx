47e34676537b22e2c322c151980e25ea468efb48
Venci <venci34@gmail.com>
2022-02-09 01:14:46 +0200

    ProductActions comp updated

--- src/components/product/ProductActions.jsx
+++ src/components/product/ProductActions.jsx
@@ -15,10 +15,25 @@ export default function ProductActions({
     <div className={classNames(styles["product-actions"])}>
       <Grid container spacing={2} columns={{ md: 10 }}>
         <Grid item xs={5}>
-          <Button className={classNames(styles.button)}>{onBuy}</Button>
+          <Button
+            className={classNames(styles.button)}
+            disabled={isLive == false ? true : false}
+            onClick={() => {
+              alert("clicked");
+            }}
+            variant="contained"
+          >
+            {onBuy}
+          </Button>
         </Grid>
         <Grid item xs={7}>
-          <Button className={classNames(styles.button)}>{onBid}</Button>
+          <Button
+            className={classNames(styles.button)}
+            variant="outlined"
+            color="success"
+          >
+            {onBid}
+          </Button>
         </Grid>
       </Grid>
     </div>

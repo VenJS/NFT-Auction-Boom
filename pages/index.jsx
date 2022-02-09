d4bbaa15c06c8dc71a670fad5e44deef049e4be3
Venci <venci34@gmail.com>
2022-02-09 18:03:58 +0200

    indexJs update

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,12 @@ import How from "../src/components/how/How";
 import Auctions from "../src/components/auctions/Auctions";
 import Footer from "../src/components/footer/Footer";
 import TopCollectors from "../src/components/collectors/TopCollectors";
+import dataActivity from "../data/activity.json";
+import dataFeatured from "../data/featured.json";
+import dataNfts from "../data/nfts.json";
+import dataProfile from "../data/profile.json";
+import dataTrending from "../data/trending.json";
+import dataUsers from "../data/users.json";
 import ProductContainer from "../src/components/product/ProductContainer/ProductContainer";
 
 export default function Index() {

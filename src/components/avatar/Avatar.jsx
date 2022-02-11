b21038310f4be07646051496e96405beab7cbfa2
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-12 01:35:11 +0200

    ProfileUser props update

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,10 +1,10 @@
 import styles from './Avatar.module.scss'
 import classNames from 'classnames'
 
-export default function Avatar({size= 90, url, verified=false}) {
+export default function Avatar({size= 90, url, verified}) {
     return (
         <div className={classNames(styles.avatar)} style={{width: size, height: size}} >
-            <img className={classNames(styles.image)} src='images/avatar.png' ></img>
+            <img className={classNames(styles.image)} src={url} ></img>
             <img className={classNames(styles.badge)} src='images/verified.svg' ver={verified}></img>
         </div>
     )
--- src/components/profile/ProfileUser.jsx
+++ src/components/profile/ProfileUser.jsx
@@ -3,10 +3,10 @@ import styles from './ProfileUser.module.scss';
 import { Typography } from "@mui/material";
 import Avatar from "../avatar/Avatar";
 
-export default function ProfileUser({name,info,avatar,verified}) {
+export default function ProfileUser({name,info,avatar='https://mir-s3-cdn-cf.behance.net/project_modules/disp/ea7a3c32163929.567197ac70bda.png',verified}) {
     return (
         <div className={classNames(styles['profile-user'])}>
-            <Avatar></Avatar>
+            <Avatar url={avatar} verified={true}></Avatar>
             <Typography variant="h3">{name}</Typography>
             <Typography variant="body1">{info}</Typography>
         </div>

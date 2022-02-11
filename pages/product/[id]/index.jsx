5e7f7a4b45b645a956fcda009be0e3a017a65ccf
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-11 21:33:11 +0200

    product folder added

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -1,7 +1,7 @@
-import Header from "../../src/components/header/Header";
-import Footer from "../../src/components/footer/Footer";
-import ProductContainer from "../../src/components/product/ProductContainer";
-import dataNfts from '../../data/nfts.json'
+import Header from "../../../src/components/header/Header";
+import Footer from "../../../src/components/footer/Footer";
+import ProductContainer from "../../../src/components/product/ProductContainer";
+import dataNfts from '../../../data/nfts.json';
 import { useRouter } from "next/router";
 
 export default function Page() {

595721ec6d993e806d9342a04104b925043c6a28
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-12 00:26:54 +0200

    How page added

---
+++ pages/how/index.jsx
@@ -0,0 +1,21 @@
+import Header from "../../src/components/header/Header";
+import Hero from "../../src/components/hero/Hero";
+import Description from "../../src/components/description/Description";
+import Footer from "../../src/components/footer/Footer";
+
+export default function How() {
+  return (
+    <div>
+      <Header></Header>
+      <Hero text="Blind Doggo"></Hero>
+      <Description
+        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+    dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+    Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+    sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue."
+        image="https://preview.redd.it/x5tdrjinpxw71.jpg?auto=webp&s=7a0e5f8dee568c7129082176c4ee661d799cfe16"
+      ></Description>
+      <Footer></Footer>
+    </div>
+  );
+}

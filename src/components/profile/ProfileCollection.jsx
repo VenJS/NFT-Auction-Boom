e0846ad446a13e7316ba9b891774f0e7247b4a32
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-12 17:39:24 +0200

    user props added

--- src/components/profile/ProfileCollection.jsx
+++ src/components/profile/ProfileCollection.jsx
@@ -9,16 +9,15 @@ import Card from '../card/Card'
 export default function ProfileCollection({user, filter, items=[1,2,3,4]}) {
     return (
         <div className={classNames(styles['profile-collection'])}>
-            <div>{user}</div>
             <Grid container>
                 <Grid item xs={3}><Typography variant="h3">Collection</Typography></Grid>
                 <Grid item xs={9}><ProfileCollectionFilters filters={filter}></ProfileCollectionFilters></Grid>
             </Grid>
             <Grid container>
-                <Grid item xs={3}><Card></Card></Grid>
-                <Grid item xs={3}><Card></Card></Grid>
-                <Grid item xs={3}><Card></Card></Grid>
-                <Grid item xs={3}><Card></Card></Grid>
+                <Grid item xs={3}><Card user={user}></Card></Grid>
+                <Grid item xs={3}><Card user={user}></Card></Grid>
+                <Grid item xs={3}><Card user={user}></Card></Grid>
+                <Grid item xs={3}><Card user={user}></Card></Grid>
                 
             </Grid>
         </div>

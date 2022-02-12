a0e96155d47536fcffa26102117a413fc1ab3e06
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-12 18:06:49 +0200

    ProfileCollection update

--- src/components/profile/ProfileCollection.jsx
+++ src/components/profile/ProfileCollection.jsx
@@ -6,18 +6,53 @@ import { Typography } from "@mui/material";
 import ProfileCollectionFilters from './ProfileCollectionFilters';
 import Card from '../card/Card'
 
-export default function ProfileCollection({user, filter, items=[1,2,3,4]}) {
+export default function ProfileCollection({user, filter, items = [{
+    name: "String",
+    likes: 0,
+    user: 'Jonny',
+    mediaUrl: "images/nft.jpg", 
+    timeLeft: 100000000,
+    price: "~12.2",
+    currency: "BTC",
+  },
+  {
+    name: "String",
+    likes: 0,
+    user: 'Ronny',
+    mediaUrl: "images/nft.jpg", 
+    timeLeft: 100000000,
+    price: "~12.2",
+    currency: "ETH",
+  },
+  {
+    name: "String",
+    likes: 0,
+    user: 'Tom',
+    mediaUrl: "images/nft.jpg", 
+    timeLeft: 100000000,
+    price: "~12.2",
+    currency: "LTC",
+  },
+  {
+    name: "String",
+    likes: 0,
+    user: 'Mike',
+    mediaUrl: "images/nft.jpg", 
+    timeLeft: 100000000,
+    price: "~12.2",
+    currency: "BNB",
+  }]}) {
     return (
         <div className={classNames(styles['profile-collection'])}>
             <Grid container>
                 <Grid item xs={3}><Typography variant="h3">Collection</Typography></Grid>
-                <Grid item xs={9}><ProfileCollectionFilters filters={filter}></ProfileCollectionFilters></Grid>
+                <Grid item xs={9}><ProfileCollectionFilters></ProfileCollectionFilters></Grid>
             </Grid>
             <Grid container>
-                <Grid item xs={3}><Card user={user}></Card></Grid>
-                <Grid item xs={3}><Card user={user}></Card></Grid>
-                <Grid item xs={3}><Card user={user}></Card></Grid>
-                <Grid item xs={3}><Card user={user}></Card></Grid>
+                <Grid item xs={3}><Card user={items[0].user} currency={items[0].currency}></Card></Grid>
+                <Grid item xs={3}><Card user={items[1].user} currency={items[1].currency}></Card></Grid>
+                <Grid item xs={3}><Card user={items[2].user} currency={items[2].currency}></Card></Grid>
+                <Grid item xs={3}><Card user={items[3].user} currency={items[3].currency}></Card></Grid>
                 
             </Grid>
         </div>

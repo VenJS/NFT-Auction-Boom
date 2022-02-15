a36dbc97d567d9a44269fa33b34d7c1ed07a769e
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-15 15:07:44 +0200

    explorefilters comp created

---
+++ src/components/explore/ExploreFilters.jsx
@@ -0,0 +1,38 @@
+import classNames from "classnames";
+import styles from "./ExploreFilters.module.scss";
+import {
+  Stack,
+  FormControl,
+  Select,
+  InputLabel,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+import Search from "@mui/icons-material/Search";
+
+export default function ExploreFilters({ filters }) {
+  return (
+    <div className={classNames(styles["explore-filters"])}>
+      <Stack>
+        <FormControl>
+          <Select autoWidth="true" label="Sort by">
+            <InputLabel>{filters}</InputLabel>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <Select label="Price range"></Select>
+        </FormControl>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search></Search>
+              </InputAdornment>
+            ),
+          }}
+          variant='standard'
+        ></TextField>
+      </Stack>
+    </div>
+  );
+}

293fcd83f950ce19f6b178a8542175d0572f3630
Ventsislav Kiskinov <venci34@gmail.com>
2022-02-15 14:25:34 +0200

    Activity page

---
+++ pages/activity/index.jsx
@@ -0,0 +1,18 @@
+import Header from '../../src/components/header/Header';
+import Hero from '../../src/components/hero/Hero';
+import ActivityFilters from '../../src/components/activity/ActivityFilters';
+import ActivityList from '../../src/components/activity/ActivityList';
+import Footer from '../../src/components/footer/Footer';
+import dataActivity from '../../data/activity.json'
+
+export default function Activity() {
+    return (
+        <div>
+            <Header></Header>
+            <Hero />
+            <ActivityFilters></ActivityFilters>
+            <ActivityList></ActivityList>
+            <Footer></Footer>
+        </div>
+    )
+}
